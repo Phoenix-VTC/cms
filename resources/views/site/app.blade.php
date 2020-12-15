9740ffbd7988b5e3ff85f6fe956c91acde5ee76a
Diego <me@diegor.nl>
2020-12-15 14:17:48 +0100

    Add page title to HTML title

--- resources/views/site/app.blade.php
+++ resources/views/site/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
-    <title>{{ config('app.name') }}</title>
+    <title>{{ $item->title }} - {{ config('app.name') }}</title>
 </head>
 <body>
 @yield('navigation')
