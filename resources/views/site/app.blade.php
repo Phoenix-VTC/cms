7ea260b6b024104bfb6c7f550a8434144c7e7615
Diego <me@diegor.nl>
2020-12-15 15:59:10 +0100

    Improve Fontawesome installation

--- resources/views/site/app.blade.php
+++ resources/views/site/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
     <title>{{ $item->title }} - {{ config('app.name') }}</title>
 </head>
