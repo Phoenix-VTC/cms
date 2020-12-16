7ce6fd12f0fa8125a0fbdee486a7d74c506d1ef0
Diego <me@diegor.nl>
2020-12-16 02:13:30 +0100

    Add text block

---
+++ resources/views/site/blocks/text.blade.php
@@ -0,0 +1,10 @@
+<?php /** @var A17\Twill\Models\Block $block */ ?>
+
+<section class="py-12 md:py-20">
+    <div class="container px-4 mx-auto">
+        <div class="mb-8">
+            <h2 class="mb-6 text-3xl md:text-4xl font-bold font-heading text-blueGray-200">{{ $block->translatedInput('title') }}</h2>
+            <div class="leading-loose text-blueGray-400">{!! $block->translatedInput('text') !!}</div>
+        </div>
+    </div>
+</section>
