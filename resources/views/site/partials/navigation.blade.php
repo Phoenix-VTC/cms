ca17703f5870d5951bda21efa158787c611bf87d
Diego <me@diegor.nl>
2020-12-16 01:56:45 +0100

    Add logo to navigation

--- resources/views/site/partials/navigation.blade.php
+++ resources/views/site/partials/navigation.blade.php
@@ -3,7 +3,7 @@
     <div class="container px-4 mx-auto">
         <nav class="flex justify-between items-center py-6">
             <a class="text-3xl text-white font-semibold leading-none" href="#">
-                <h1 class="font-bold">Phoenix</h1>
+                <img src="{{ asset('assets/images/branding/logo.svg') }}" alt="Phoenix" width="100">
             </a>
             <div class="lg:hidden">
                 <button
