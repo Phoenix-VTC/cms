d9df997f488d12b9bb88fa8bce4a604c1567413c
Diego <me@diegor.nl>
2020-12-20 16:11:44 +0100

    Add Google Analytics

--- resources/views/site/app.blade.php
+++ resources/views/site/app.blade.php
@@ -8,6 +8,20 @@
     <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
     <title>{{ $item->title }} - {{ config('app.name') }}</title>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-R59FJ1SJVV"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'G-R59FJ1SJVV');
+    </script>
 </head>
 <body class="antialiased bg-body text-body font-body bg-gray-900">
 @yield('navigation')
