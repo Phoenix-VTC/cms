972ea01708f484473dbf980f2a16a5769aafe716
Diego <me@diegor.nl>
2021-01-07 21:38:38 +0100

    Fix sentence

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto pt-4 px-6 pb-6 mb-8 bg-gray-800 text-white rounded">
         <p class="font-bold font-heading">Notice</p>
         <p class="mt-3 mb-6 text-blueGray-200 text-sm">
-            We and selected partners, use cookies or similar technologies as specified in the <a class="hover:text-blueGray-400" href="{{ url("$locale/cookie-policy") }}">cookie policy</a>
+            We and selected partners, use cookies or similar technologies as specified in the <a class="hover:text-blueGray-400" href="{{ url("$locale/cookie-policy") }}">cookie policy</a>.
             You can consent to the use of such technologies by closing this notice.
         </p>
         <a class="inline-block py-3 px-6 mr-2 text-xs leading-none bg-orange-600 hover:bg-orange-700 rounded js-cookie-consent-agree cookie-consent__agree" href="">{{ trans('cookieConsent::texts.agree') }}</a>
