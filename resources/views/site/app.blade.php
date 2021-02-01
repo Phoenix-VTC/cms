15d30dbae3f57f7a36550e860dd983606f22391f
Diego <me@diegor.nl>
2021-02-01 20:54:37 +0100

    Add some primary meta tags and organize head

--- resources/views/site/app.blade.php
+++ resources/views/site/app.blade.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    {{-- Primary Meta Tags --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>{{ $item->title }} - {{ config('app.name') }}</title>
+    <meta name="title" content="{{ $item->title }} - {{ config('app.name') }}">
+    <meta name="description"
+          content="Phoenix is a brand-new VTC, founded by experienced members of the community. We believe in forward thinking, and strive to put our members first!">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- Favicons --}}
     <link rel="apple-touch-icon" sizes="180x180"
           href="{{ asset('assets/images/branding/favicons/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32"
@@ -15,12 +22,11 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="msapplication-config" content="{{ asset('browserconfig.xml') }}">
     <meta name="theme-color" content="#18181B">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- Stylesheets --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
-    <title>{{ $item->title }} - {{ config('app.name') }}</title>
-
+    {{-- Google Analytics --}}
     @if(\Request::hasCookie('phoenix_cookie_consent'))
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-R59FJ1SJVV"></script>
         <script>
