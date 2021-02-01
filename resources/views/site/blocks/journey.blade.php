0f9aa0fcd5eb9d237e0f5415424e7c3923c606c3
Diego <me@diegor.nl>
2021-02-01 16:53:58 +0100

    Remove if statement

--- resources/views/site/blocks/journey.blade.php
+++ resources/views/site/blocks/journey.blade.php
@@ -2,30 +2,26 @@
 
 <section class="py-20">
     <div class="container px-4 mx-auto">
-        @if($block->input('journey-type') === 'horizontal')
-            <div class="max-w-lg mx-auto mb-16 text-center">
+        <div class="max-w-lg mx-auto mb-16 text-center">
             <span class="inline-block py-1 px-3 text-xs font-semibold bg-secondary text-white rounded-xl">
                 {{ $block->translatedInput('label') }}
             </span>
-                <h2 class="mt-4 mb-2 text-3xl lg:text-4xl font-bold font-heading text-white">
-                    <span>{{ $block->translatedInput('title') }}</span>
-                </h2>
-                <p class="text-blueGray-400 leading-loose">
-                    {{ $block->translatedInput('description') }}
-                </p>
+            <h2 class="mt-4 mb-2 text-3xl lg:text-4xl font-bold font-heading text-white">
+                <span>{{ $block->translatedInput('title') }}</span>
+            </h2>
+            <p class="text-blueGray-400 leading-loose">
+                {{ $block->translatedInput('description') }}
+            </p>
+        </div>
+        <div class="flex relative">
+            <div class="hidden md:flex absolute inset-0 -mt-24 px-16 items-center justify-center max-w-4xl mx-auto">
+                <div class="w-full py-1 bg-white"></div>
             </div>
-            <div class="flex relative">
-                <div class="hidden md:flex absolute inset-0 -mt-24 px-16 items-center justify-center max-w-4xl mx-auto">
-                    <div class="w-full py-1 bg-white"></div>
-                </div>
-                <div class="flex flex-wrap justify-between w-full">
-                    @foreach($block->children as $block)
-                        @include('site.blocks.journey-step', $block)
-                    @endforeach
-                </div>
+            <div class="flex flex-wrap justify-between w-full">
+                @foreach($block->children as $block)
+                    @include('site.blocks.journey-step', $block)
+                @endforeach
             </div>
-        @else
-            {{-- TODO: Other journey option --}}
-        @endif
+        </div>
     </div>
 </section>
