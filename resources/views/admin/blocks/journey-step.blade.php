88626782b67b03ea5ab420ecf5c683f025439cc6
Diego <me@diegor.nl>
2021-02-01 16:42:00 +0100

    Create journey repeater block

---
+++ resources/views/admin/blocks/journey-step.blade.php
@@ -0,0 +1,15 @@
+@twillBlockTitle('Journey Step')
+@twillBlockIcon('text')
+@twillBlockGroup('app')
+
+@formField('input', [
+    'name' => 'title',
+    'label' => 'Title',
+    'translated' => true,
+])
+
+@formField('input', [
+    'name' => 'text',
+    'label' => 'Text',
+    'translated' => true,
+])
