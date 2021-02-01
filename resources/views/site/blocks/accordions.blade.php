10e613087012b2394de9b67c447893613e5d18ff
Diego <me@diegor.nl>
2021-02-01 15:57:18 +0100

    Create accordion repeater block

---
+++ resources/views/site/blocks/accordions.blade.php
@@ -0,0 +1,19 @@
+<?php /** @var A17\Twill\Models\Block $block */ ?>
+
+<section class="py-20 lg:bg-contain bg-top bg-no-repeat">
+    <div class="container mx-auto px-4">
+        <div class="max-w-sm mx-auto mb-12 text-center">
+            <h2 class="mb-4 text-3xl md:text-4xl font-bold font-heading text-white">{{ $block->translatedInput('title') }}</h2>
+            <div class="text-sm md:text-base leading-loose text-blueGray-400">
+                {!! $block->translatedInput('description') !!}
+            </div>
+        </div>
+        <div class="max-w-3xl mx-auto">
+            <ul class="space-y-4">
+                @foreach($block->children as $block)
+                    @include('site.blocks.accordion', $block)
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</section>
