db3e324a83284661651b055370999d0e6340a16f
iDiego <me@diegor.nl>
2021-02-01 19:58:15 +0100

    Fix partners Background Color (#26)

--- resources/views/site/blocks/partners.blade.php
+++ resources/views/site/blocks/partners.blade.php
@@ -1,6 +1,6 @@
 <?php /** @var A17\Twill\Models\Block $block */ ?>
 
-<section class="py-20 pb-8 overflow-x-hidden">
+<section class="py-20 pb-8 overflow-x-hidden {{ "bg-" . $block->input('background_color') }}">
     <div class="container px-4 mx-auto">
         <h1 class="my-3 pb-8 text-3xl md:text-5xl text-white font-bold font-heading text-center">
             {{ $block->translatedInput('title') }}
