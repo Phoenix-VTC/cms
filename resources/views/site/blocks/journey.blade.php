7379c08311105a52cbdc4b591a5a3681cb012f14
Diego <me@diegor.nl>
2021-02-02 15:44:17 +0100

    Add background_color to journey

--- resources/views/site/blocks/journey.blade.php
+++ resources/views/site/blocks/journey.blade.php
@@ -1,6 +1,6 @@
 <?php /** @var A17\Twill\Models\Block $block */ ?>
 
-<section class="py-20">
+<section class="py-20 {{ "bg-" . $block->input('background_color') }}">
     <div class="container px-4 mx-auto">
         <div class="max-w-lg mx-auto mb-16 text-center">
             <span class="inline-block py-1 px-3 text-xs font-semibold bg-secondary text-white rounded-xl">
