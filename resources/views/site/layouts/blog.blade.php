2317b9e8546ad141253000f716e38e742ee71abb
Diego <me@diegor.nl>
2021-02-04 14:35:19 +0100

    Populate blog layout blade

--- resources/views/site/layouts/blog.blade.php
+++ resources/views/site/layouts/blog.blade.php
@@ -1,1 +1,13 @@
-Blog Layout
+@extends('site.app')
+
+@section('navigation')
+    @include('site.partials.navigation')
+@endsection
+
+@section('contents')
+    @include("site.modules.blog")
+@endsection
+
+@section('footer')
+    @include('site.partials.footer')
+@endsection
