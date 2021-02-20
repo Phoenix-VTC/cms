6a616479b9f128974e1ed1891ee2c805f59ddff4
Diego <me@diegor.nl>
2021-02-20 18:31:25 +0100

    Change img height to auto and change the bottom margin

--- resources/views/site/blocks/image.blade.php
+++ resources/views/site/blocks/image.blade.php
@@ -1,7 +1,7 @@
 <?php /** @var A17\Twill\Models\Block $block */ ?>
 
 <div class="mb-8">
-    <img class="h-80 mx-auto mb-6 object-cover rounded" src="{{ asset($block->image('image')) }}"
+    <img class="h-auto mx-auto mb-2 object-cover rounded" src="{{ asset($block->image('image')) }}"
          alt="{{ $block->imageAltText('image') }}">
     <p class="text-sm text-center italic text-blueGray-400">{{ $block->imageCaption('image') }}</p>
 </div>
