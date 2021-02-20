89c34ab88740e0717256582360925b1b483ba317
Diego <me@diegor.nl>
2021-02-20 17:47:52 +0100

    Remove publish_start_date and publish_end_date from the blogs migration

--- database/migrations/2021_02_04_130416_create_blogs_tables.php
+++ database/migrations/2021_02_04_130416_create_blogs_tables.php
@@ -13,9 +13,6 @@ class CreateBlogsTables extends Migration
 
             $table->integer('position')->unsigned()->nullable();
 
-            // add those 2 columns to enable publication timeframe fields (you can use publish_start_date only if you don't need to provide the ability to specify an end date)
-            $table->timestamp('publish_start_date')->nullable();
-            $table->timestamp('publish_end_date')->nullable();
 
             $table->string('author', 200)->nullable();
         });
