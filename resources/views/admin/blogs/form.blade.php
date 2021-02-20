bea5877ea617fdf7b994b0329a0bb67aabc13bcd
Diego <me@diegor.nl>
2021-02-20 18:14:35 +0100

    Remove maxlength from blog description

--- resources/views/admin/blogs/form.blade.php
+++ resources/views/admin/blogs/form.blade.php
@@ -22,8 +22,7 @@
     @formField('input', [
         'name' => 'description',
         'label' => 'Description',
-        'translated' => true,
-        'maxlength' => 100
+        'translated' => true
     ])
 
     @formField('block_editor')
