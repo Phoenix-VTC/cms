75a6fd0927a3b5dc436299eaad7c35d9398f008f
Diego <me@diegor.nl>
2021-02-20 18:30:49 +0100

    Replace imageAltText with imageCaption

--- resources/views/site/blocks/image.blade.php
+++ resources/views/site/blocks/image.blade.php
@@ -3,5 +3,5 @@
 <div class="mb-8">
     <img class="h-80 mx-auto mb-6 object-cover rounded" src="{{ asset($block->image('image')) }}"
          alt="{{ $block->imageAltText('image') }}">
-    <p class="text-sm text-center text-blueGray-400">{{ $block->imageAltText('image') }}</p>
+    <p class="text-sm text-center italic text-blueGray-400">{{ $block->imageCaption('image') }}</p>
 </div>
