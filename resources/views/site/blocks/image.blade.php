478cd25f7e0ed774efbd0f60749e7240c315509e
Diego <me@diegor.nl>
2021-02-20 18:34:34 +0100

    Add a element to image

--- resources/views/site/blocks/image.blade.php
+++ resources/views/site/blocks/image.blade.php
@@ -1,7 +1,9 @@
 <?php /** @var A17\Twill\Models\Block $block */ ?>
 
 <div class="mb-8">
-    <img class="h-auto mx-auto mb-2 object-cover rounded" src="{{ asset($block->image('image')) }}"
-         alt="{{ $block->imageAltText('image') }}">
+    <a target="_blank" href="{{ $block->image('image') }}">
+        <img class="h-auto mx-auto mb-2 object-cover rounded" src="{{ asset($block->image('image')) }}"
+             alt="{{ $block->imageAltText('image') }}">
+    </a>
     <p class="text-sm text-center italic text-blueGray-400">{{ $block->imageCaption('image') }}</p>
 </div>
