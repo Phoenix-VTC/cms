ac54f6f549079da59e63f559d00b30839787a674
Diego <me@diegor.nl>
2021-02-20 17:47:13 +0100

    Add image, author_image, author and block_editor to the blogs form

--- resources/views/admin/blogs/form.blade.php
+++ resources/views/admin/blogs/form.blade.php
@@ -1,10 +1,30 @@
 @extends('twill::layouts.form')
 
 @section('contentFields')
+    @formField('medias', [
+        'name' => 'image',
+        'label' => 'Header image',
+    ])
+
+    @formField('medias', [
+        'name' => 'author_image',
+        'translated' => false,
+        'label' => 'Author image',
+    ])
+
+    @formField('input', [
+        'name' => 'author',
+        'label' => 'Author',
+        'translated' => false,
+        'maxlength' => 100
+    ])
+
     @formField('input', [
         'name' => 'description',
         'label' => 'Description',
         'translated' => true,
         'maxlength' => 100
     ])
+
+    @formField('block_editor')
 @stop
